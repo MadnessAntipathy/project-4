e9626270befc5fdbfd007b1e751d93ed811c18e0
marcus <marcusteo@hotmail.com>
2019-09-11 15:00:37 +0800

    removed old build

--- build/server/routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app, db) => {
-  const main = require('./controllers/main')(db);
-  app.post('/create', main.create);
-  app.post('/login', main.login);
-  app.post('/score', main.score);
-  app.post('/submitscore', main.submit);
-
-};
