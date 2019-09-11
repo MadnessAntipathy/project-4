5ce578d827900c1a1cd5fac63f713b6577f246b5
marcus <marcusteo@hotmail.com>
2019-09-11 15:05:41 +0800

    copied build files

---
+++ build/server/tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  password TEXT);
+
+CREATE TABLE IF NOT EXISTS scores (
+  id SERIAL PRIMARY KEY,
+  userid INTEGER,
+  scores INTEGER,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
