a2567bbe487df3d7f625462fbb1a1be3f701b318
marcus <marcusteo@hotmail.com>
2019-09-12 11:24:20 +0800

    fixed bug in user last direction not being read

--- src/server/logic/logic.js
+++ src/server/logic/logic.js
@@ -30,49 +30,52 @@ module.exports.spawnEnemy = function(speed, multiplier){
     x: Math.floor(Math.random()*500),
     y: Math.floor(Math.random()*500)
   }
-  if (multiplier % 5 === 4){
-    if (!objects["alertState"]){
-      objects["alertState"] = alertState
-    }
-    objects["alertState"].color = "yellow"
-  }else if (multiplier % 5 === 0){
-    objects["alertState"].color = "purple"
-  }else {
-    if (objects["alertState"]){
-      delete objects["alertState"]
-    }
-  }
+
+  // if (multiplier % 5 === 4){
+  //   if (!objects["alertState"]){
+  //     objects["alertState"] = alertState
+  //   }
+  //   objects["alertState"].color = "yellow"
+  // }else if (multiplier % 5 === 0){
+  //   objects["alertState"].color = "purple"
+  // }else {
+  //   if (objects["alertState"]){
+  //     delete objects["alertState"]
+  //   }
+  // }
 
   var playerList = Object.values(objects).filter(obj => obj.type === "player")
   for (var i = 0; i < multiplier; i++){
     var enemyObject = returnEnemyList(speed,playerList)
     objects[enemyObject.id] = enemyObject.enemy
   }
-  if (multiplier % 5 === 0){
-    for (var j = 0; j < playerList.length; j++){
-      var seekerObject = returnSeeker()
-      objects[seekerObject.id] = seekerObject
-      if (playerList[j]){
-        objects[seekerObject.id].target = playerList[j]
-      }
-    }
-  }
+  // if (multiplier % 5 === 0){
+  //   for (var j = 0; j < playerList.length; j++){
+  //     var seekerObject = returnSeeker()
+  //     objects[seekerObject.id] = seekerObject
+  //     if (playerList[j]){
+  //       objects[seekerObject.id].target = playerList[j]
+  //     }
+  //   }
+  // }
 }
 
-function returnSeeker (speed){
-  var randNum = Math.floor(Math.random()*1000000)
-  return {
-    id: randNum,
-    type: "enemy",
-    direction: "seeker",
-    speed: 1,
-    x: objects["alertState"].x,
-    y: objects["alertState"].y,
-    target: "playerList[Math.floor(Math.random()*playerList.length)]",
-    move: 0,
-    distance: 500
-  }
-}
+
+//
+// function returnSeeker (speed){
+//   var randNum = Math.floor(Math.random()*1000000)
+//   return {
+//     id: randNum,
+//     type: "enemy",
+//     direction: "seeker",
+//     speed: 1,
+//     x: objects["alertState"].x,
+//     y: objects["alertState"].y,
+//     target: "playerList[Math.floor(Math.random()*playerList.length)]",
+//     move: 0,
+//     distance: 500
+//   }
+// }
 
 function returnEnemyList (speed,playerList){
   var randNum = Math.floor(Math.random()*1000000)
@@ -231,76 +234,77 @@ module.exports.detectCollision = function (){
       for (var j = 0; j < array.length; j++){
         if (array[j].type === 'player' && array[i].id !== array[j].id){
           if (array[i].x+10 > array[j].x && array[i].x < array[j].x+10 && array[i].y+10 > array[j].y && array[i].y < array[j].y+10){
+            if (objects[array[i].id] && objects[array[j].id]){
+              if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "up"){
+                objects[array[i].id].y += 10
+                objects[array[j].id].y -= 20
 
-            if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "up"){
-              objects[array[i].id].y += 10
-              objects[array[j].id].y -= 20
+              }else if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "down"){
+                objects[array[i].id].y += 20
+                objects[array[j].id].y -= 20
 
-            }else if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "down"){
-              objects[array[i].id].y += 20
-              objects[array[j].id].y -= 20
+              }else if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "left"){
+                objects[array[i].id].y += 10
+                objects[array[j].id].y -= 20
 
-            }else if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "left"){
-              objects[array[i].id].y += 10
-              objects[array[j].id].y -= 20
+              }else if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "right"){
+                objects[array[i].id].y += 10
+                objects[array[j].id].y -= 20
 
-            }else if (objects[array[i].id].lastDirection === "up" && objects[array[j].id].lastDirection === "right"){
-              objects[array[i].id].y += 10
-              objects[array[j].id].y -= 20
+              }
 
-            }
+              if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "up"){
+                objects[array[i].id].y -= 20
+                objects[array[j].id].y += 20
 
-            if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "up"){
-              objects[array[i].id].y -= 20
-              objects[array[j].id].y += 20
+              }else if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "down"){
+                objects[array[i].id].y -= 10
+                objects[array[j].id].y += 20
 
-            }else if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "down"){
-              objects[array[i].id].y -= 10
-              objects[array[j].id].y += 20
+              }else if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "left"){
+                objects[array[i].id].y -= 10
+                objects[array[j].id].y += 20
 
-            }else if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "left"){
-              objects[array[i].id].y -= 10
-              objects[array[j].id].y += 20
+              }else if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "right"){
+                objects[array[i].id].y -= 10
+                objects[array[j].id].y += 20
 
-            }else if (objects[array[i].id].lastDirection === "down" && objects[array[j].id].lastDirection === "right"){
-              objects[array[i].id].y -= 10
-              objects[array[j].id].y += 20
+              }
 
-            }
+              if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "up"){
+                objects[array[i].id].x += 10
+                objects[array[j].id].x -= 20
 
-            if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "up"){
-              objects[array[i].id].x += 10
-              objects[array[j].id].x -= 20
+              }else if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "down"){
+                objects[array[i].id].x += 10
+                objects[array[j].id].x -= 20
 
-            }else if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "down"){
-              objects[array[i].id].x += 10
-              objects[array[j].id].x -= 20
+              }else if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "left"){
+                objects[array[i].id].x += 10
+                objects[array[j].id].x -= 20
 
-            }else if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "left"){
-              objects[array[i].id].x += 10
-              objects[array[j].id].x -= 20
+              }else if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "right"){
+                objects[array[i].id].x += 20
+                objects[array[j].id].x -= 20
 
-            }else if (objects[array[i].id].lastDirection === "left" && objects[array[j].id].lastDirection === "right"){
-              objects[array[i].id].x += 20
-              objects[array[j].id].x -= 20
+              }
 
-            }
+              if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "up"){
+                objects[array[i].id].x -= 10
+                objects[array[j].id].x += 20
 
-            if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "up"){
-              objects[array[i].id].x -= 10
-              objects[array[j].id].x += 20
+              }else if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "down"){
+                objects[array[i].id].x -= 10
+                objects[array[j].id].x += 20
 
-            }else if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "down"){
-              objects[array[i].id].x -= 10
-              objects[array[j].id].x += 20
+              }else if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "left"){
+                objects[array[i].id].x -= 20
+                objects[array[j].id].x += 20
 
-            }else if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "left"){
-              objects[array[i].id].x -= 20
-              objects[array[j].id].x += 20
-
-            }else if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "right"){
-              objects[array[i].id].x -= 10
-              objects[array[j].id].x += 20
+              }else if (objects[array[i].id].lastDirection === "right" && objects[array[j].id].lastDirection === "right"){
+                objects[array[i].id].x -= 10
+                objects[array[j].id].x += 20
+              }
             }
           }
         }else if (array[j].type === 'enemy'){
